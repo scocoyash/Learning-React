2a2148da1aa4c60b8743190b4542fa588aaf7b79
scocoyash <yashjain864@gmail.com>
2020-08-20 02:31:03 +0530

    [burger-builder] completed project

---
+++ BurgerBuilder/src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Logo.css';
+import burgerLogo from '../../assets/images/burger-logo.png';
+
+const logo = (props) => (
+    <div className={classes.Logo}>
+        <img src={burgerLogo} alt="MyBurger" 
+            style={{height:props.height}}
+        />
+    </div>      
+);
+
+export default logo;
